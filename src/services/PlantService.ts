a8a40afdcd9246f41f04b993e27ea091cb731012
Theocrsb <theo_crosbie@yahoo.fr>
2022-11-12 14:41:04 +0100

    ajout UPDATE

--- src/services/PlantService.ts
+++ src/services/PlantService.ts
@@ -3,17 +3,17 @@ import Plant from "../models/interfaces/PlantInterface";
 
 class PlantService {
   getAllPlants(): Promise<Plant[]> {
-    return AppDataSource.query(`SELECT * FROM plant;`);
+    return AppDataSource.query(`SELECT * FROM plant`);
   }
   getOnePlant(id: number): Promise<Plant[]> {
     console.log(id);
-    return AppDataSource.query(`SELECT * FROM plant where id=${id};`);
+    return AppDataSource.query(`SELECT * FROM plant where id=${id}`);
   }
   createOnePlant(body: Plant): Promise<Plant[]> {
     console.log(body);
     return AppDataSource.query(
       `insert into plant (name, unitprice_ati, quantity, category, url_picture)
-       values ('${body.name}', '${body.unitprice_ati}', '${body.quantity}', '${body.category}', '${body.url_picture}';)`
+       values ('${body.name}', '${body.unitprice_ati}', '${body.quantity}', '${body.category}', '${body.url_picture}')`
     );
   }
   deletePlant(id: number): Promise<Plant[]> {
